3d2038e31a4f216ea546dd94b8df9d3bbcb23f21
monirulislamkhan <mr.monirulkhan@gmail.com>
2024-10-17 11:05:20 +0530

    Right now working on dom section

--- iteration-array-methods.js
+++ iteration-array-methods.js
@@ -92,6 +92,36 @@ const social = ['Youtube', 'Linkedin', 'facebook', 'Twitter', 'Pinterest', 'Inst
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // x = numbers.filter((number) => number / 2 === 0);
 
+// const companies = [
+//   { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+//   { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+//   { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+//   { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+//   { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+//   { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+//   { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+//   { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+//   { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
+// ];
+
+// const retailCom = companies.filter((company) => company.category === 'Retail');
+
+// const earlyCom = companies.filter((company) => company.start >= 1980 && company.end <= 2005);
+
+// const longCom = companies.filter((company) => company.end - company.start >= 10);
+
+// console.log(longCom);
+
+// Arrays Map
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// x = numbers.map((number) => number * 3);
+
+// forEach Method
+// const doubleNum = [];
+// numbers.forEach((number) => {
+//   doubleNum.push(number *)
+// });
+
 const companies = [
   { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
   { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
@@ -104,10 +134,92 @@ const companies = [
   { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
 ];
 
-// const retailCom = companies.filter((company) => company.category === 'Retail');
+// const companyName = companies.map((company) => company.name);
 
-// const earlyCom = companies.filter((company) => company.start >= 1980 && company.end <= 2005);
+// console.log(companyName);
 
-const longCom = companies.filter((company) => company.end - company.start >= 10);
+// Create an array of objects with name and the length of each company in years
+// const companyInfo = companies.map((company) => {
+//   return {
+//     name: company.name,
+//     companyAge: company.end - company.start + ' Years',
+//   };
+// });
 
-console.log(longCom);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Chain map methods
+// const squareAndDouble = numbers.map((number) => Math.sqrt(number)).map((sqrt) => sqrt + 3);
+
+// console.log(squareAndDouble);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const sum = numbers.reduce((acc, value) => {
+//   return acc + value;
+// });
+
+// console.log(sum);
+
+// const cart = [
+//   { id: 1, name: 'Product 1', price: 130 },
+//   { id: 2, name: 'Product 2', price: 150 },
+//   { id: 3, name: 'Product 3', price: 200 },
+// ];
+
+// const totalPrice = cart.reduce((prevValue, curValue) => {
+//   return prevValue + curValue.price;
+// }, 0);
+// console.log(totalPrice);
+
+// Challenge 1
+// const people = [
+//   {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     email: 'john@gmail.com',
+//     phone: '111-111-1111',
+//     age: 30,
+//   },
+//   {
+//     firstName: 'Jane',
+//     lastName: 'Poe',
+//     email: 'jane@gmail.com',
+//     phone: '222-222-2222',
+//     age: 25,
+//   },
+//   {
+//     firstName: 'Bob',
+//     lastName: 'Foe',
+//     email: 'bob@gmail.com',
+//     phone: '333-333-3333',
+//     age: 45,
+//   },
+//   {
+//     firstName: 'Sara',
+//     lastName: 'Soe',
+//     email: 'Sara@gmail.com',
+//     phone: '444-444-4444',
+//     age: 19,
+//   },
+//   {
+//     firstName: 'Jose',
+//     lastName: 'Koe',
+//     email: 'jose@gmail.com',
+//     phone: '555-555-5555',
+//     age: 23,
+//   },
+// ];
+
+// const youngPeople = people
+//   .filter((young) => young.age <= 25)
+//   .map((info) => ({
+//     name: info.firstName + ' ' + info.lastName,
+//     email: info.email,
+//   }));
+
+// console.log(youngPeople);
+
+// const numbers = [2, -30, 50, 20, -12, -9, 7];
+// const positiveNum = numbers.filter((pos) => pos > 0).reduce((prev, cur) => prev + cur, 0);
+
+// console.log(positiveNum);
