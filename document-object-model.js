7ce9adf44a9e8c767125c41dbb9e179cc602bed0
monirulislamkhan <mr.monirulkhan@gmail.com>
2024-10-17 20:46:13 +0530

    working on dom

--- document-object-model.js
+++ document-object-model.js
@@ -23,3 +23,39 @@
 // const customSelector = document.querySelector('#h1');
 // console.log(customSelector);
 // customSelector.innerText = 'Hi';
+// console.log(document.querySelectorAll('*').length);
+let output;
+// Everything in all html tags
+output = document.head;
+output = document.body;
+
+output = document.head.children;
+output = document.body.children;
+output = document.doctype;
+output = document.domain;
+output = location.hostname;
+output = location.host;
+output = document.URL;
+output = document.characterSet;
+output = document.contentType;
+// Forms
+output = document.querySelector('.form-control');
+// output = document.forms.children;
+output = document.forms[0].id;
+output = document.forms[0].method;
+output = document.forms[0].action;
+
+// Change id
+output = document.forms[0].id = 'test';
+// Get all links
+output = document.links;
+output = document.links[0];
+output = document.links[0].href;
+// output = document.links[0].href = 'https://www.google.com';
+// output = document.links[0].textContent = 'Hello';
+output = document.links[0].id = 'google-link';
+
+// output = document.links[0].className = 'nirul';
+output = document.links[0].classList;
+
+console.log(output);
